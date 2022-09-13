98b87d868a23dd0e187731300db51127c0f2ff0b
Febin Shaji <72760226+FebinShaji@users.noreply.github.com>
2022-09-13 16:24:04 +0100

    Add files via upload

---
+++ build/CMakeFiles/chatbot.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/csunix/ed192fs/Desktop/Programming_Project/comp1921_2021/CWK2/ingredients.c" "/home/csunix/ed192fs/Desktop/Programming_Project/comp1921_2021/CWK2/build/CMakeFiles/chatbot.dir/ingredients.c.o"
+  "/home/csunix/ed192fs/Desktop/Programming_Project/comp1921_2021/CWK2/main.c" "/home/csunix/ed192fs/Desktop/Programming_Project/comp1921_2021/CWK2/build/CMakeFiles/chatbot.dir/main.c.o"
+  "/home/csunix/ed192fs/Desktop/Programming_Project/comp1921_2021/CWK2/menu.c" "/home/csunix/ed192fs/Desktop/Programming_Project/comp1921_2021/CWK2/build/CMakeFiles/chatbot.dir/menu.c.o"
+  "/home/csunix/ed192fs/Desktop/Programming_Project/comp1921_2021/CWK2/seating.c" "/home/csunix/ed192fs/Desktop/Programming_Project/comp1921_2021/CWK2/build/CMakeFiles/chatbot.dir/seating.c.o"
+  "/home/csunix/ed192fs/Desktop/Programming_Project/comp1921_2021/CWK2/staff.c" "/home/csunix/ed192fs/Desktop/Programming_Project/comp1921_2021/CWK2/build/CMakeFiles/chatbot.dir/staff.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/chatbot.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/chatbot.dir/main.c.o"
+  "CMakeFiles/chatbot.dir/menu.c.o"
+  "CMakeFiles/chatbot.dir/ingredients.c.o"
+  "CMakeFiles/chatbot.dir/staff.c.o"
+  "CMakeFiles/chatbot.dir/seating.c.o"
+  "chatbot.pdb"
+  "chatbot"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/chatbot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
